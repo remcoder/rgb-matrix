85a9229d4537d058f19dc64f3d6648bd259798c9
Remco Veldkamp <remcoder@gmail.com>
2015-01-26 22:18:36 +0100

    added Grapnel router

--- webcontrol/client/main.next.js
+++ webcontrol/client/main.next.js
@@ -4,3 +4,50 @@ RegisterUnderscoreHelpers();
 UI.registerHelper('formatNumber', function(number, formatting) {
   return numeral(number).format(formatting);
 });
+
+UI.registerHelper('arduino', function() {
+  return Arduinos.findOne();
+});
+
+
+// setup router
+var router = new Grapnel({ pushState : true });
+var mainTemplate = new ReactiveVar();
+
+// define routes
+router.get('/', function(req){
+  router.navigate('/sequencer');
+});
+
+router.get('/generator', function(req){
+  mainTemplate.set('generator');
+  console.log('load live-coding template')
+});
+
+router.get('/sequencer', function(req){
+  mainTemplate.set('sequencer');
+  console.log('load sequencer template')
+});
+
+router.get('/console', function(req){
+  mainTemplate.set('console');
+  console.log('load sequencer template')
+});
+
+router.get('/draw', function(req){
+  mainTemplate.set('draw');
+  console.log('load draw template')
+});
+
+router.get('/image', function(req){
+  mainTemplate.set('image');
+  console.log('load image template')
+});
+
+Template.main.helpers({
+  mainTemplate : function() {
+    return mainTemplate.get();
+  }
+});
+
+
--- webcontrol/client/views/draw/bitmask.js
+++ webcontrol/client/views/draw/bitmask.js
@@ -1,8 +1,13 @@
 
 Template.bitmask.helpers({
+  matrix : function() {
+    return Matrix.findOne();
+  },
+
   pixels : function() {
     var pixels = [];
 
+    
     this.bitmask.forEach(function(r, i) {
       r.split('').forEach(function(val, j) {
         pixels.push({
@@ -45,3 +50,37 @@ Template.bitmask.events({
     });
   }
 });
+
+
+Template.bitmask.events({
+  'change input[type=color]' : _.throttle(function(evt) {
+  // console.log(evt.currentTarget.value);
+    Matrix.update('42', { $set:
+      { color: hex2rgb(evt.currentTarget.value) }
+    });
+  },50)
+});
+
+Template.bitmask.helpers({
+  hexColor : function(c) {
+    return rgbToHex(c[0], c[1], c[2]);
+  }
+});
+
+
+
+function hex2rgb(h){return [hexToR(h),hexToG(h),hexToB(h)]; };
+function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
